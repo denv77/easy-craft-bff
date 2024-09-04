6ee6aba75ca01e50acd61342a03e867ebbef13d9
den <denis.vorobjew@yandex.ru>
2024-09-04 17:12:26 +0300

    cors

--- src/main/java/ru/easycraft/bff/controller/BffController.java
+++ src/main/java/ru/easycraft/bff/controller/BffController.java
@@ -8,32 +8,34 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
 
 import java.net.URI;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin(originPatterns = "http://localhost:[*]", allowCredentials = "true") // Только для DEV
 @Slf4j
 public class BffController {
 
 
     @GetMapping(value = "/bff/manifest", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String manifest(@CookieValue(value = "access_token", required = false) String accessToken) {
+    public Mono<String> manifest(@CookieValue(value = "access_token", required = false) String accessToken) {
         log.info("GET manifest. Cookie access_token: '{}'", accessToken);
 
         if (!StringUtils.hasText(accessToken)) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
         }
 
-        return """
+        return Mono.just("""
                 {
                   "mfe" : "http://localhost:4201/remoteEntry.json"
-                }""";
+                }""");
     }
 
     @GetMapping(value = "/bff/login")
-    public void login(@RequestParam(required = true) String redirectUrl, ServerHttpResponse response) {
-        log.info("GET login. redirectUrl: '{}'", redirectUrl);
+    public Mono<Void> login(@RequestParam(name = "redirect_uri") String redirectUri, ServerHttpResponse response) {
+        log.info("GET login. redirect_uri: '{}'", redirectUri);
 
         response.addCookie(
                 ResponseCookie.from("access_token", "123")
@@ -41,22 +43,22 @@ public class BffController {
                         .path("/")
                         .build());
         response.setStatusCode(HttpStatus.FOUND);
-        response.getHeaders().setLocation(URI.create(redirectUrl));
-//        return response.setComplete();
+        response.getHeaders().setLocation(URI.create(redirectUri));
+        return response.setComplete();
     }
 
     @GetMapping(value = "/sso/userinfo", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String userinfo(@CookieValue(value = "access_token", required = false) String accessToken) {
+    public Mono<String> userinfo(@CookieValue(value = "access_token", required = false) String accessToken) {
         log.info("GET userinfo. Cookie access_token: '{}'", accessToken);
 
         if (!StringUtils.hasText(accessToken)) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
         }
-        return """
+        return Mono.just("""
                 {
-                  "id" : "777"
+                  "id" : "777",
                   "name" : "Анастасия"
-                }""";
+                }""");
     }
 
 
